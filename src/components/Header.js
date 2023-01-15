4f5957f64e65c478bed6607622388a4b07315815
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-01-14 23:22:38 -0500

    final touches

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({handleToggleDarkMode}) => {
     return (
         <div className = "header">
             <h1>Notes</h1>
-            <button onClick={() => handleToggleDarkMode((previousMode)=> !previousMode)} className='save'>Toggle Mode</button>
+            <button onClick={() => handleToggleDarkMode((previousMode)=> !previousMode)} className='save'>Dark/Light</button>
         </div>
     )
 }
