7063eb334956203c6ec3d14e52d85c85a140b8d6
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-01-14 23:40:25 -0500

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Header = ({handleToggleDarkMode}) => {
     return (
         <div className = "header">
-            <h1>Notes</h1>
+            <h1>Write some note to me :) </h1> 
             <button onClick={() => handleToggleDarkMode((previousMode)=> !previousMode)} className='save'>Dark/Light</button>
         </div>
+       
     )
 }
 
